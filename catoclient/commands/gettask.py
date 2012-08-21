e816ec37a8f50705a5419b8a01236436b89bf7a2
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2012-08-21 17:01:32 -0400

    New commands for getting a task, parameter xml document, and parameter help.

---
+++ catoclient/commands/gettask.py
@@ -0,0 +1,36 @@
+#########################################################################
+# Copyright 2011 Cloud Sidekick
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#########################################################################
+
+import catoclient.catocommand
+from catoclient.param import Param
+
+class GetTask(catoclient.catocommand.CatoCommand):
+
+    Description = 'Gets a Task object.'
+    Options = [Param(name='task', short_name='t', long_name='task',
+                     optional=False, ptype='string',
+                     doc='The ID or Name of a Task.'),
+               Param(name='version', short_name='v', long_name='version',
+                     optional=True, ptype='string',
+                     doc='An optional specific Task Version. (Default if omitted.)'),
+              Param(name='include_code', short_name='v', long_name='include_code',
+                     optional=True, ptype='boolean',
+                     doc='Include all code.')]
+
+    def main(self):
+        results = self.call_api('taskMethods/get_task', ['task', 'version', 'include_code'])
+        print(results)
+
