60584d1eb854e702d873c7fa1a04660753e034f6
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2013-03-13 10:09:45 -0400

    fix tagging script

--- tag_release_and_bump.sh
+++ tag_release_and_bump.sh
@@ -8,7 +8,7 @@ if [ "$version" = "" ] ; then
 fi
 git pull
 sed -i "" -e "s|^__version__=.*$|__version__='${version}'|" catoclient/__init__.py
-sed -i "" -e "s|^    version=.*$|    version='${version}'|" setup.py
+sed -i "" -e "s|^    version=.*$|    version='${version},'|" setup.py
 echo $version > VERSION
 
 git add VERSION catoclient/__init__.py setup.py
