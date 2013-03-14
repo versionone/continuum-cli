76feed2bf7fc134ee1a253a2c59000b249343f1f
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-03-14 11:18:38 -0400

    delete-task command

---
+++ catoclient/commands/deletetask.py
@@ -0,0 +1,39 @@
+#########################################################################
+# Copyright 2011 Cloud Sidekick
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#########################################################################
+
+import catoclient.catocommand
+from catoclient.param import Param
+
+class DeleteTask(catoclient.catocommand.CatoCommand):
+
+    Description = 'Deletes a Cato Task.'
+    Options = [Param(name='task', short_name='t', long_name='task',
+                     optional=False, ptype='string',
+                     doc='The ID or Name of the Task to delete.')]
+
+    def main(self):
+        go = False
+        if self.force:
+            go = True
+        else:
+            answer = raw_input("WARNING: This is a utility function.\n\nDeleting a Task cannot be undone.\n\nAre you sure? ")
+            if answer:
+                if answer.lower() in ['y', 'yes']:
+                    go = True
+
+        if go:
+            results = self.call_api('taskMethods/delete_task', ['task'])
+            print(results)
