9efc3ae7a79d2536a40d5e4ef9d61bce4b89985e
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-03-14 11:44:39 -0400

    get-service-instances command

---
+++ maestroclient/commands/getserviceinstances.py
@@ -0,0 +1,35 @@
+#########################################################################
+# 
+# Copyright 2013 Cloud Sidekick
+# __________________
+# 
+#  All Rights Reserved.
+# 
+# NOTICE:  All information contained herein is, and remains
+# the property of Cloud Sidekick and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Cloud Sidekick
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Cloud Sidekick.
+#
+#########################################################################
+
+import catoclient.catocommand
+from catoclient.param import Param
+
+class GetServiceInstances(catoclient.catocommand.CatoCommand):
+
+    Description = 'Gets Service Instances from an Deployment Service'
+    Options = [Param(name='deployment', short_name='d', long_name='deployment',
+                     optional=False, ptype='string',
+                     doc='Value can be either an deployment id or deployment name.'),
+                 Param(name='service', short_name='v', long_name='service',
+                     optional=False, ptype='string',
+                     doc='Value can be either a service id or service name.')]
+
+    def main(self):
+        results = self.call_api('depMethods/get_service_instances', ['deployment', 'service'])
+        print(results)
