35badf1248f065e769e8c83da7cacfb9e6484f91
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-04-15 12:47:56 -0400

    new commands - get-cloud-account and create-cloud-account.

---
+++ catoclient/commands/createcloudaccount.py
@@ -0,0 +1,45 @@
+#########################################################################
+# Copyright 2011 Cloud Sidekick
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#########################################################################
+
+import catoclient.catocommand
+from catoclient.param import Param
+
+class CreateCloudAccount(catoclient.catocommand.CatoCommand):
+
+    Description = 'Creates a new Cloud Account.'
+    Options = [Param(name='provider', short_name='v', long_name='provider',
+                     optional=False, ptype='string',
+                     doc='The name of a Cato supported Cloud Provider.'),
+               Param(name='name', short_name='n', long_name='name',
+                     optional=False, ptype='string',
+                     doc='A name for the new Cloud Account.'),
+               Param(name='login', short_name='l', long_name='login',
+                     optional=False, ptype='string',
+                     doc='Login name (Access Key) for the new Cloud Account.'),
+               Param(name='password', short_name='p', long_name='password',
+                     optional=False, ptype='string',
+                     doc='Password for the new Cloud Account.'),
+               Param(name='default_cloud', short_name='d', long_name='default_cloud',
+                     optional=False, ptype='string',
+                     doc='A default Cloud to be associated with this Account.'),
+               Param(name='account_number', short_name='a', long_name='account_number',
+                     optional=True, ptype='string',
+                     doc='An account number for the New Cloud Account.')
+               ]
+
+    def main(self):
+        results = self.call_api('cloudMethods/create_account', ['provider', 'name', 'login', 'password', 'default_cloud', 'account_number'])
+        print(results)
