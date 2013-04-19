8bdee4ed2d3264cbae18bf6ada73fb3602d0adc4
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-04-19 10:52:30 -0400

    Commands for Maestro ticket # 38 - list/copy templates.

---
+++ maestroclient/commands/copydeploymenttemplate.py
@@ -0,0 +1,42 @@
+#########################################################################
+# 
+# Copyright 2013 Cloud Sidekick
+# __________________
+# 
+#  All Rights Reserved.
+# 
+# NOTICE:  All information contained herein is, and remains
+# the property of Cloud Sidekick and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Cloud Sidekick
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Cloud Sidekick.
+#
+#########################################################################
+
+import catoclient.catocommand
+from catoclient.param import Param
+
+class CopyDeploymentTemplate(catoclient.catocommand.CatoCommand):
+
+    Description = 'Copies a Deployment Template.'
+    Options = [Param(name='template', short_name='t', long_name='template',
+                     optional=False, ptype='string',
+                     doc='The Deployment Template to copy.'),
+               Param(name='version', short_name='v', long_name='version',
+                     optional=False, ptype='string',
+                     doc='A version for the new Template.'),
+               Param(name='newname', short_name='nn', long_name='newname',
+                     optional=False, ptype='string',
+                     doc='A name for the new Template.'),
+               Param(name='newversion', short_name='nv', long_name='newversion',
+                     optional=False, ptype='string',
+                     doc='A version for the new Template.')
+               ]
+
+    def main(self):
+        results = self.call_api('depMethods/copy_deployment_template', ['newname', 'newversion', 'template', 'version'])
+        print(results)
