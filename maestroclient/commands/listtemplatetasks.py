6451f320b9b9c633514a23dac7ba5c9a604b2b48
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-06-04 20:52:01 -0400

    list-application-template-tasks command

---
+++ maestroclient/commands/listtemplatetasks.py
@@ -0,0 +1,35 @@
+#########################################################################
+# 
+# Copyright 2013 Cloud Sidekick
+# __________________
+# 
+#  All Rights Reserved.
+# 
+# NOTICE:  All information contained herein is, and remains
+# the property of Cloud Sidekick and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Cloud Sidekick
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Cloud Sidekick.
+#
+#########################################################################
+
+import catoclient.catocommand
+from catoclient.param import Param
+
+class ListTemplateTasks(catoclient.catocommand.CatoCommand):
+
+    Description = 'Lists all Tasks associated with an Application Template'
+    Options = [Param(name='template', short_name='t', long_name='template',
+                     optional=False, ptype='string',
+                     doc='Name of the Application Template.'),
+               Param(name='version', short_name='v', long_name='version',
+                     optional=False, ptype='string',
+                     doc='The Application Template Version.')]
+
+    def main(self):
+        results = self.call_api('depMethods/list_template_tasks', ['template', 'version'])
+        print(results)
