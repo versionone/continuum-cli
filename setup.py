746db86e23d7b2c88a832bb30260b482dc604cca
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-06-06 11:08:16 -0400

    bump release to 1.16

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ for f in os.listdir("bin"):
 
 setuptools.setup(
     name='maestroclient',
-    version='1.15',
+    version='1.16',
     description='Cloud Sidekick Maestro Client',
     license='Proprietary License',
     author='Patrick Dunnigan',
