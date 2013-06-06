4ab4832a1af36319db0cf5e4f70530e79d73a6bb
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-06-06 11:15:19 -0400

    updated tag script

--- tag_release_and_bump.sh
+++ tag_release_and_bump.sh
@@ -6,9 +6,16 @@ if [ "$version" = "" ] ; then
     echo "usage: $0 <release>"
     exit 1
 fi
+
+# 'latest' version
+git tag -d latest
+git push origin :refs/tags/latest
+
+# supplied version
 git tag -d $version
 git push origin :refs/tags/$version
 git pull
+
 sed -i "" -e "s|^__version__=.*$|__version__='${version}'|" catoclient/__init__.py
 sed -i "" -e "s|^    version=.*$|    version='${version}',|" setup.py
 echo $version > VERSION
@@ -16,5 +23,7 @@ echo $version > VERSION
 git add VERSION catoclient/__init__.py setup.py
 git commit -m "bump release to ${version}"
 git push
+
+git tag -a latest -m "Version $version"
 git tag -a $version -m "Version $version"
 git push --tags
