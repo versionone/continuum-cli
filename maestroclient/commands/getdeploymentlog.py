96aadd3f913a00b7e1276e909ab11dad2fabd05a
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-06-12 14:46:29 -0400

    Tweaked help for get-deployment-log

--- maestroclient/commands/getdeploymentlog.py
+++ maestroclient/commands/getdeploymentlog.py
@@ -25,7 +25,7 @@ class GetDeploymentLog(catoclient.catocommand.CatoCommand):
     Description = 'Get the log for a Deployment.'
     Options = [Param(name='deployment', short_name='d', long_name='deployment',
                      optional=False, ptype='string',
-                     doc='Value can be either an deployment id or deployment name.'),
+                     doc='Value can be either a Deployment ID or Name.'),
                Param(name='filter', short_name='f', long_name='filter',
                      optional=True, ptype='string',
                      doc='A filter.'),
