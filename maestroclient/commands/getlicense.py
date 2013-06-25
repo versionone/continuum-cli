2babaf4f820a88b0c8243823955207ca2e1b0c46
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-06-25 16:52:15 -0400

    install-license and get-license commands

---
+++ maestroclient/commands/getlicense.py
@@ -0,0 +1,30 @@
+#########################################################################
+# 
+# Copyright 2013 Cloud Sidekick
+# __________________
+# 
+#  All Rights Reserved.
+# 
+# NOTICE:  All information contained herein is, and remains
+# the property of Cloud Sidekick and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Cloud Sidekick
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Cloud Sidekick.
+#
+#########################################################################
+
+import catoclient.catocommand
+from catoclient.param import Param
+
+class GetLicense(catoclient.catocommand.CatoCommand):
+
+    Description = 'Get the current license.'
+    Options = []
+
+    def main(self):
+        results = self.call_api('depMethods/get_license', [])
+        print(results)
