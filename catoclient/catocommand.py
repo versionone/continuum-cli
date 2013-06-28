27b49c5e4f7046bb0550bfe19ad7492b8a982fc2
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2013-06-28 14:21:38 -0400

    removed iterfind depends in ET library

--- catoclient/catocommand.py
+++ catoclient/catocommand.py
@@ -43,11 +43,6 @@ try:
     import xml.etree.cElementTree as ET
 except (AttributeError, ImportError):
     import xml.etree.ElementTree as ET
-try:
-    ET.ElementTree.iterfind
-except AttributeError as ex:
-    del(ET)
-    import catoxml.etree.ElementTree as ET
 
 
 class CatoCommand(object):
