013f2323731ac4e26eeea2b17fd16250ae301e60
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2013-09-16 17:02:38 -0400

    bump release to 1.20

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ for f in os.listdir("bin"):
 
 setuptools.setup(
     name='catoclient',
-    version='1.19',
+    version='1.20',
     description='Cloud Sidekick Cato Community Edition (CE) Catoclient',
     license='Apache License (2.0)',
     author='Patrick Dunnigan',
