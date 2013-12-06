0b9a822606e8cac9495ed70adc66de7b18df0a0e
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-12-06 12:43:04 -0500

    changed the version number

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ for f in os.listdir("bin"):
 
 setuptools.setup(
     name='maestroclient',
-    version='1.21',
+    version='1.22',
     description='Cloud Sidekick Maestro Client',
     license='Proprietary License',
     author='Patrick Dunnigan',
