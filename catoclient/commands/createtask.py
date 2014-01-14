4202eb32a2c3be2dfc504e6e3ee0e82f8289f1eb
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-01-14 16:57:29 -0500

    updated create task example

--- catoclient/commands/createtask.py
+++ catoclient/commands/createtask.py
@@ -19,9 +19,11 @@ from catoclient.param import Param
 
 class CreateTask(catoclient.catocommand.CatoCommand):
 
-    Description = 'Creates a new Task.'
+    Description = 'Creates a new blank Task object in Cato.'
     API = 'create_task'
-    Examples = ''''''
+    Examples = '''
+        cato-create-task -n "mytask" -d "This is a sample task" -c "test2001"
+    '''
     Options = [Param(name='name', short_name='n', long_name='name',
                      optional=False, ptype='string',
                      doc='A name for the new Task.'),
