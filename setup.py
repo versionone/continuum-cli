adb3c7b311a8d825932ee53dd813614c180c303d
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-05-27 12:29:21 -0400

    bump release to 1.25

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ for f in os.listdir("bin"):
 
 setuptools.setup(
     name='catoclient',
-    version='1.24',
+    version='1.25',
     description='Cloud Sidekick Cato Community Edition (CE) Client',
     license='Apache License (2.0)',
     author='Patrick Dunnigan',
