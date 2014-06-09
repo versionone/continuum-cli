e5bb133d8deaa90e330ea839fe3f027036eb99a2
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-06-09 10:32:46 -0400

    bump release to 1.26

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ for f in os.listdir("bin"):
 
 setuptools.setup(
     name='catoclient',
-    version='1.25',
+    version='1.26',
     description='Cloud Sidekick Client Tools',
     license='Apache License (2.0)',
     author='Patrick Dunnigan',
