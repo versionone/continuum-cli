b5d0fa6b1ba88bbc775b7887a7e38186c858d9e0
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-06-27 12:34:57 -0400

    bump release to 1.30

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ for f in os.listdir("bin"):
 
 setuptools.setup(
     name='maestroclient',
-    version='1.27',
+    version='1.30',
     description='Cloud Sidekick Client Tools',
     license='Proprietary License',
     author='Patrick Dunnigan',
