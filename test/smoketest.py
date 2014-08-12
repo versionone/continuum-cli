ddb0e32bba7ac3d5eb69f503413f2ee157d2957b
shannoncruey <shannon@AluminumOverdrive.local>
2014-08-12 17:04:19 -0400

    Added a smoke test for all commands.

---
+++ test/smoketest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+
+Inspired by the routine that builds command documentation.  This executes each command
+to try and get the docs.  Any errors will cause a crash.
+
+Execute each command with the --dumpdoc flag to get the documentation.
+
+"""
+
+import os
+import sys
+import subprocess
+
+
+def eval_commands(listcmd):
+    # B) build a dict of every command (using our existing commands that do this!)
+    cmdlst = subprocess.check_output(listcmd, shell=True).split("\n")
+    
+    # execute each command with the --dumpdoc flag
+    for c in cmdlst:
+        if c:
+            print "... testing [%s]" % (c)
+            subprocess.check_output("%s --dumpdoc" % (c), shell=True)
+            
+
+print "Testing commands..."
+eval_commands("csk-list-commands")
+print "SUCCESS"
