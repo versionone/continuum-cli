7f2e664a0c8df535626c1ad2a148646b3a992109
shannoncruey <shannon@AluminumOverdrive.local>
2014-11-07 16:36:09 -0500

    new commands to add deployment/service/instance

--- cskcommands/deploy/addserviceinstance.py
+++ cskcommands/deploy/addserviceinstance.py
@@ -20,43 +20,28 @@
 import cskcommands.cmd
 from cskcommands.param import Param
 
-class AddService(cskcommands.cmd.CSKCommand):
+class AddServiceInstance(cskcommands.cmd.CSKCommand):
 
-    Description = 'Creates a new Deployment Service.'
-    API = 'add_service'
+    Description = 'Creates a new Deployment Service Instance.'
+    API = 'add_service_instance'
     Examples = ''''''
     Options = [Param(name='deployment', short_name='d', long_name='deployment',
                      optional=False, ptype='string',
                      doc='Value can be either a Deployment ID or Name.'),
+               Param(name='service', short_name='s', long_name='service',
+                     optional=False, ptype='string',
+                     doc='Value can be either a Service ID or Name.'),
                Param(name='name', short_name='n', long_name='name',
                      optional=False, ptype='string',
-                     doc='Name for the new Deployment.'),
-               Param(name='servicefile', short_name='s', long_name='servicefile',
+                     doc='Name (Label) of the new Instance.'),
+               Param(name='hostname', short_name='h', long_name='hostname',
                      optional=True, ptype='string',
-                     doc='Additional properties in JSON Service definition format.')
+                     doc='Real host name.'),
+               Param(name='address', short_name='a', long_name='address',
+                     optional=True, ptype='string',
+                     doc='Host IP Address (required with `hostname`.')
                ]
 
     def main(self):
-        go = False
-        if self.force:
-            go = True
-        else:
-            answer = raw_input("\nAdding a Service to a running Deployment makes it different from the source Application Template.\n\nAre you sure? ")
-            if answer:
-                if answer.lower() in ['y', 'yes']:
-                    go = True
-
-        if go:
-            self.service = None
-            if self.servicefile:
-                import os
-                fn = os.path.expanduser(self.servicefile)
-                with open(fn, 'r') as f_in:
-                    if not f_in:
-                        print("Unable to open file [%s]." % fn)
-                    data = f_in.read()
-                    if data:
-                        self.service = data
-
-            results = self.call_api(self.API, ['deployment', 'name', 'service'])
-            print(results)
+        results = self.call_api(self.API, ['deployment', 'service', 'name', 'hostname', 'address'])
+        print(results)
