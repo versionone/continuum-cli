28f33f0f9ead51299a12cfe7654d21374049f756
shannoncruey <shannon@AluminumOverdrive.local>
2015-01-16 17:18:39 -0500

    copyright updates

--- cskcommands/deploy/installlicense.py
+++ cskcommands/deploy/installlicense.py
@@ -1,20 +1,8 @@
 #########################################################################
 # 
-# Copyright 2013 Cloud Sidekick
-# __________________
-# 
-#  All Rights Reserved.
-# 
-# NOTICE:  All information contained herein is, and remains
-# the property of ClearCode Labs and its suppliers,
-# if any.  The intellectual and technical concepts contained
-# herein are proprietary to ClearCode Labs
-# and its suppliers and may be covered by U.S. and Foreign Patents,
-# patents in process, and are protected by trade secret or copyright law.
-# Dissemination of this information or reproduction of this material
-# is strictly forbidden unless prior written permission is obtained
-# from ClearCode Labs.
-#
+# Copyright 2015 ClearCode Labs
+# All Rights Reserved.
+# http://www.clearcodelabs.com/license.html
 #########################################################################
 
 import cskcommands.cmd
