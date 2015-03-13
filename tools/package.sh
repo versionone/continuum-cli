15efef89c24a55449feb4af6b59c9b207e0bc53b
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2015-03-13 12:53:47 -0400

    create install package for client tools in windows, CLIEN-2

--- tools/package.sh
+++ tools/package.sh
@@ -8,7 +8,11 @@ mkdir -p artifact/${PRODUCT}
 cp -R  README.md LICENSE bin cskcommands setup.py artifact/${PRODUCT}
 cd artifact 
 tar -czf ${FILENAME}.tar.gz ${PRODUCT}
+find bin -type f -exec mv {} {}.py
+tar -czf ${FILENAME}-win.tar.gz ${PRODUCT}
 . ~/.awscreds
 s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${FILENAME}.tar.gz
+s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${FILENAME}-win.tar.gz
 set +x
 echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${FILENAME}.tar.gz\"}]"
+echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}-win\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${FILENAME}-win.tar.gz\"}]"
