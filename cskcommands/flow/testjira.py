862ffe25c063d6f6604e0f110879be998e69809f
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2015-05-27 12:02:37 -0400

    jira plugin changed to support multiple instances, FLOW-277

--- cskcommands/flow/testjira.py
+++ cskcommands/flow/testjira.py
@@ -12,9 +12,13 @@ class TestJira(cskcommands.cmd.CSKCommand):
     Description = "Test Jira connectivity"
     API = "test_jira"
     Examples = """
-    ccl-testjira
+    ccl-testjira -i instancename
 """
+    Options = [Param(name='instance', short_name='i', long_name='instance',
+                     optional=True, ptype='string',
+                     doc='Jira instance name in the ClearCode configuration. Optional, do not use if testing default Jira instance.'),
+               ]
 
     def main(self):
-        results = self.call_api(self.API, [])
+        results = self.call_api(self.API, ['instance'])
         print(results)
