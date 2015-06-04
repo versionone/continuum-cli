671a53ae216aff2d9a610e3740d0d4cb24147bfe
shannoncruey <shannon@AluminumOverdrive.local>
2015-06-04 14:12:06 -0400

    FLOW-284 - command to increment instance autonumber seed

---
+++ cskcommands/flow/setpigroupnumber.py
@@ -0,0 +1,44 @@
+#########################################################################
+#
+# Copyright 2015 ClearCode Labs
+# All Rights Reserved.
+# http://www.clearcodelabs.com/license.html
+#
+#
+#########################################################################
+
+import cskcommands.cmd
+from cskcommands.param import Param
+
+
+class SetPIGroupNumber(cskcommands.cmd.CSKCommand):
+
+    Description = """Set the autonumber root on Pipeline Instance Group. (Used to issue ascending numbers to Instances in the Group.)
+
+Requires the '_id' of the Pipeline Instance Group and the new number value to set.
+
+Returns success or failure."""
+
+    API = 'set_pipelinegroup_number'
+    Examples = ''''''
+    Options = [Param(name='pg', short_name='i', long_name='pg',
+                     optional=False, ptype='string',
+                     doc='ID of a Pipeline Instance Group.'),
+               Param(name='newnumber', short_name='n', long_name='newnumber',
+                     optional=False, ptype='string',
+                     doc='ID of a Pipeline Instance Group.')               
+               ]
+
+    def main(self):
+        go = False
+        if self.force:
+            go = True
+        else:
+            answer = raw_input("Are you sure (y/n)? ")
+            if answer:
+                if answer.lower() in ['y', 'yes']:
+                    go = True
+
+        if go:
+            results = self.call_api(self.API, ['pg', 'newnumber'])
+            print(results)
