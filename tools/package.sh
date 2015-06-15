f69880188cec31bd072c90a53c6bce878de09429
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2015-06-15 12:31:01 -0400

    new packaging model

--- tools/package.sh
+++ tools/package.sh
@@ -10,9 +10,13 @@ cd artifact
 tar -czf ${FILENAME}.tar.gz ${PRODUCT}
 find ${PRODUCT}/bin -type f -exec mv {} {}.py \;
 tar -czf ${FILENAME}-win.tar.gz ${PRODUCT}
+mkdir ${WORKSPACE}/${PRODUCT}
+mv ${FILENAME}.tar.gz ${WORKSPACE}/${PRODUCT}/.
+mv ${FILENAME}-win.tar.gz ${WORKSPACE}/${PRODUCT}/.
+cd $WORKSPACE
 . ~/.awscreds
-s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${FILENAME}.tar.gz
-s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${FILENAME}-win.tar.gz
+s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${PRODUCT}/${FILENAME}.tar.gz
+s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${PRODUCT}/${FILENAME}-win.tar.gz
 set +x
-echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${FILENAME}.tar.gz\"}]"
-echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}-win\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${FILENAME}-win.tar.gz\"}]"
+echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${PRODUCT}/${FILENAME}.tar.gz\"}]"
+echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}-win\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${PRODUCT}/${FILENAME}-win.tar.gz\"}]"
