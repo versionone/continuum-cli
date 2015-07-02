17449cd4fd062cee7c74117816adf05e1a87bb27
shannoncruey <shannon@AluminumOverdrive.local>
2015-07-02 15:02:19 -0400

    DEPL-343 - ability to pass data to a sequence for all tasks

--- cskcommands/deploy/runsequence.py
+++ cskcommands/deploy/runsequence.py
@@ -23,6 +23,14 @@ _To run a sequence while passing in parameters stored in a file_
 
     ccl-run-sequence -d "MyApp20" -s "Start" -p "./myapp_parms.json"
 
+_To run a sequence with 'initial data' as a JSON string. (Notice double quotes inside, single quote outside.)_
+
+    ccl-run-sequence -d "MyApp20" -s "Start" --data '{"ship":"Serenity","captain":"Malcolm Reynolds"}'
+
+_To run a sequence with 'initial data' for each Step read from a file_
+
+    ccl-run-sequence -d "MyApp20" -s "Start" --data "./stepdata.json"
+
 _To run a sequence that should halt instead of pause on error, also not prompt to continue_
 
     ccl-run-sequence -d "MyApp20" -s "Start" -e "halt" --force
@@ -39,7 +47,10 @@ _To run a sequence that should halt instead of pause on error, also not prompt t
                      doc='The on_error directive for this Sequence. ("pause" or "halt", "pause" if omitted)'),
                Param(name='parameters', short_name='p', long_name='parameters',
                      optional=True, ptype='string',
-                     doc='JSON or XML formatted parameters, or a path to a file containing JSON or XML parameters.')
+                     doc='JSON or XML formatted parameters, or a path to a file containing JSON or XML parameters.'),
+               Param(name='data', long_name='data',
+                     optional=True, ptype='string',
+                     doc='JSON object, or a path to a file containing a JSON object.'),
                ]
 
     def main(self):
@@ -53,6 +64,24 @@ _To run a sequence that should halt instead of pause on error, also not prompt t
                     go = True
 
         if go:
+            # first validate any initial 'data'
+            if self.data:
+                try:
+                    import json
+                    json.loads(self.data)
+                except:  
+                    # nope, maybe it's a file path
+                    try:
+                        import os
+                        fn = os.path.expanduser(self.data)
+                        with open(fn, 'r') as f_in:
+                            if not f_in:
+                                print("Unable to open data file [%s]." % fn)
+                            self.parameters = f_in.read()
+                    except:  
+                        # well, nothing worked so let's just whine
+                        print ("'data' argument was provided, but unable to reconcile data as JSON or an existing and valid file.")
+
             # first, check if the "parameters" argument is json, xml, or a filename.
             if self.parameters:
                 try:
@@ -73,5 +102,5 @@ _To run a sequence that should halt instead of pause on error, also not prompt t
                         except:  # well, nothing worked so let's just whine
                             print ("'parameters' argument was provided, but unable to reconcile parameters as JSON, XML or a valid and existing file.")
 
-            results = self.call_api(self.API, ['deployment', 'sequence', 'onerror', 'parameters'])
+            results = self.call_api(self.API, ['deployment', 'sequence', 'onerror', 'parameters', 'data'])
             print(results)
