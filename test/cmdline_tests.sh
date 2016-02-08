fab1cff7610d1df56dd790531b5a7a19d2e95d30
shannoncruey <shannon@cruey.net>
2016-02-07 23:25:02 -0700

    tests now ignore pep8 E402

--- test/cmdline_tests.sh
+++ test/cmdline_tests.sh
@@ -9,7 +9,7 @@ find . -name "*.py" | xargs pylint --rcfile=test/pylint.conf -E --disable=E0203,
 find bin -type f | xargs pylint --rcfile=test/pylint.conf -E --disable=E0203,E1101,E1103,E0611,E0102,E0702 >> test/test_output/pylint.txt
 
 echo "running pep8 tests ..."
-pep8 --ignore=W291,E501,W293,E302,E303,E203,E121,W191,E128,E251,E101,E301,E122,E261,E231,E123,E127,W391,E211 . | sed 's/^..//' > test/test_output/pep8.txt
-pep8 --ignore=W291,E501,W293,E302,E303,E203,E121,W191,E128,E251,E101,E301,E122,E261,E231,E123,E127,W391,E211 bin/*  >> test/test_output/pep8.txt
+pep8 --ignore=E402,W291,E501,W293,E302,E303,E203,E121,W191,E128,E251,E101,E301,E122,E261,E231,E123,E127,W391,E211 . | sed 's/^..//' > test/test_output/pep8.txt
+pep8 --ignore=E402,W291,E501,W293,E302,E303,E203,E121,W191,E128,E251,E101,E301,E122,E261,E231,E123,E127,W391,E211 bin/*  >> test/test_output/pep8.txt
 
 echo "command line tests completed"
