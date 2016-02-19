954c1bcf5bfba426c9252518dfca248213d9c082
shannoncruey <shannon@cruey.net>
2016-02-19 13:49:54 -0500

    updated packaging for new bucket

--- tools/package.sh
+++ tools/package.sh
@@ -6,7 +6,7 @@ PRODUCT=client
 FILENAME=${PRODUCT}-${BUILD_NUMBER}
 mkdir -p artifact/${PRODUCT}
 cp -R  README.md LICENSE bin cskcommands setup.py artifact/${PRODUCT}
-cd artifact 
+cd artifact
 tar -czf ${FILENAME}.tar.gz ${PRODUCT}
 find ${PRODUCT}/bin -type f -exec mv {} {}.py \;
 tar -czf ${FILENAME}-win.tar.gz ${PRODUCT}
@@ -15,9 +15,9 @@ mv ${FILENAME}.tar.gz ${WORKSPACE}/${PRODUCT}/.
 mv ${FILENAME}-win.tar.gz ${WORKSPACE}/${PRODUCT}/.
 cd $WORKSPACE
 . ~/.awscreds
-s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${PRODUCT}/${FILENAME}.tar.gz
-s3put -b builds.clearcodelabs.com --grant public-read --prefix ${PWD}/ ${PRODUCT}/${FILENAME}-win.tar.gz
+s3put -b builds.continuum.versionone.com --grant public-read --prefix ${PWD}/ ${PRODUCT}/${FILENAME}.tar.gz
+s3put -b builds.continuum.versionone.com --grant public-read --prefix ${PWD}/ ${PRODUCT}/${FILENAME}-win.tar.gz
 set +x
-echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${PRODUCT}/${FILENAME}.tar.gz\"}]"
-echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}-win\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${PRODUCT}/${FILENAME}-win.tar.gz\"}]"
+echo "[V1ARTIFACT:{\"name\": \"${PRODUCT}\", \"location\": \"https://s3.amazonaws.com/builds.continuum.versionone.com/${PRODUCT}/${FILENAME}.tar.gz\"}]"
+echo "[V1ARTIFACT:{\"name\": \"${PRODUCT}-win\", \"location\": \"https://s3.amazonaws.com/builds.continuum.versionone.com/${PRODUCT}/${FILENAME}-win.tar.gz\"}]"
 echo "done"
