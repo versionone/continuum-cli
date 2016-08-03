1c9ef0482aca6203fa134bc13b9a0273fc709342
Patrick Dunnigan <patrick.dunnigan@versionone.com>
2016-08-03 18:43:02 -0400

    bumped release to 16.2.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for f in os.listdir("bin"):
 
 setuptools.setup(
     name='continuumclient',
-    version='16.2.0',
+    version='16.2.3',
     description='VersionOne Client Tools',
     author='Patrick Dunnigan',
     author_email='patrick.dunnigan@versionone.com',
