1b124f0d1f120fb4b41b2b3aad54f2542a57f5e5
Shannon Cruey <shannon@cruey.net>
2019-02-21 13:15:34 -0500

    S-67435 - hopefully fixed the issue with upload.sh

--- tools/upload.sh
+++ tools/upload.sh
@@ -7,8 +7,8 @@ BRANCH=$3
 WORKSPACE=$PWD
 OUTDIR=${WORKSPACE}/outputfiles
 
-mv ${OUTDIR}/client-win.zip ${OUTDIR}/client/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip
-mv ${OUTDIR}/client.tar.gz ${OUTDIR}/client/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz
+mv ${OUTDIR}/client-win.zip ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip
+mv ${OUTDIR}/client.tar.gz ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz
 
-aws s3 cp ${OUTDIR}/client/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip s3://versionone-builds/continuum/client/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip --acl public-read --quiet
-aws s3 cp ${OUTDIR}/client/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz s3://versionone-builds/continuum/client/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz --acl public-read --quiet
+aws s3 cp ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip s3://versionone-builds/continuum/client/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip --acl public-read --quiet
+aws s3 cp ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz s3://versionone-builds/continuum/client/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz --acl public-read --quiet
