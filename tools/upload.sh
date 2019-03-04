b37d08c3a98c90d21e472aecda13a6bdcb8ac58f
Shannon Cruey <shannon@cruey.net>
2019-03-04 11:58:16 -0500

    upload script now uses ContinuumCI env vars

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
-VERSION=$1
-BUILD=$2
 # branch will be nothing blank if master branch
-BRANCH=$3
+BRANCH=$1
 WORKSPACE=$PWD
 OUTDIR=${WORKSPACE}/outputfiles
 
-mv ${OUTDIR}/client-win.zip ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip
-mv ${OUTDIR}/client.tar.gz ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz
+VERSION=${CI_PROJECT_VERSION}.${CI_JOB_NUMBER}${BRANCH}
 
-aws s3 cp ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip s3://versionone-builds/continuum/client/continuum-client-${VERSION}.${BUILD}${BRANCH}-windows.zip --acl public-read --quiet
-aws s3 cp ${OUTDIR}/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz s3://versionone-builds/continuum/client/continuum-client-${VERSION}.${BUILD}${BRANCH}.tar.gz --acl public-read --quiet
+mv ${OUTDIR}/client-win.zip ${OUTDIR}/continuum-client-${VERSION}-windows.zip
+mv ${OUTDIR}/client.tar.gz ${OUTDIR}/continuum-client-${VERSION}.tar.gz
+
+aws s3 cp ${OUTDIR}/continuum-client-${VERSION}-windows.zip s3://versionone-builds/continuum/client/continuum-client-${VERSION}-windows.zip --acl public-read --quiet
+aws s3 cp ${OUTDIR}/continuum-client-${VERSION}.tar.gz s3://versionone-builds/continuum/client/continuum-client-${VERSION}.tar.gz --acl public-read --quiet
