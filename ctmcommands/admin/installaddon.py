618f6ad80b19a88fc6d656b3e0195e32cbca4229
Kevin Marsh <kmarsh@collab.net>
2019-10-14 13:51:55 -0400

    S-68247 cli commamd to install an add-on from the vs-exchange

---
+++ ctmcommands/admin/installaddon.py
@@ -0,0 +1,32 @@
+#########################################################################
+#
+# Copyright 2019 VersionOne
+# All Rights Reserved.
+# http://www.versionone.com
+#
+#
+#########################################################################
+
+import ctmcommands.cmd
+from ctmcommands.param import Param
+
+
+class InstallAddOn(ctmcommands.cmd.CSKCommand):
+
+    Description = """Installs an Add-On from the VS-Exchange
+
+Returns success or an error message."""
+
+    API = 'install_add_on'
+    Examples = ''''''
+    Options = [Param(name='name', short_name='n', long_name='name',
+                     optional=False, ptype='string',
+                     doc='The name of the add-on to install from the VS-Exchange'),
+               Param(name='team', short_name='t', long_name='team',
+                     optional=False, ptype='string',
+                     doc="""The name of the team in which to install any projects, packages, pipelines, and tasks included in this add-on"""),
+               ]
+
+    def main(self):
+        results = self.call_api(self.API, ['name', 'team'])
+        print(results)
