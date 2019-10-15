a044b0bcdd78845f4c4558f84bc48a14a20bf1a0
Kevin Marsh <kmarsh@collab.net>
2019-10-15 13:11:43 -0400

    S-68954 new command to uninstall an add-on

---
+++ ctmcommands/admin/uninstalladdon.py
@@ -0,0 +1,29 @@
+#########################################################################
+#
+# Copyright 2019 VersionOne
+# All Rights Reserved.
+# http://www.versionone.com
+#
+#
+#########################################################################
+
+import ctmcommands.cmd
+from ctmcommands.param import Param
+
+
+class UninstallAddOn(ctmcommands.cmd.CSKCommand):
+
+    Description = """Uninstalls an Add-On from the VS-Exchange
+
+Returns success or an error message."""
+
+    API = 'uninstall_add_on'
+    Examples = ''''''
+    Options = [Param(name='name', short_name='n', long_name='name',
+                     optional=False, ptype='string',
+                     doc='The name of the add-on to uninstall from Continuum'),
+               ]
+
+    def main(self):
+        results = self.call_api(self.API, ['name'])
+        print(results)
