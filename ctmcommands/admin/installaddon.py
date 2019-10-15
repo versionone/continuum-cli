e7cadea35e610da1671272d014e9878519793266
Kevin Marsh <kmarsh@collab.net>
2019-10-15 12:32:55 -0400

    S-68247 Team is only required iff add-on contians team assets

--- ctmcommands/admin/installaddon.py
+++ ctmcommands/admin/installaddon.py
@@ -23,8 +23,9 @@ Returns success or an error message."""
                      optional=False, ptype='string',
                      doc='The name of the add-on to install from the VS-Exchange'),
                Param(name='team', short_name='t', long_name='team',
-                     optional=False, ptype='string',
-                     doc="""The name of the team in which to install any projects, packages, pipelines, and tasks included in this add-on"""),
+                     optional=True, ptype='string',
+                     doc="""The name of the team in which to install any projects, packages, pipelines, and tasks included in this add-on.
+                            Required if any any projects, packages, pipelines, or tasks exist in the add-on"""),
                ]
 
     def main(self):
