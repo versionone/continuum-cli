db99a6d61a3e0a2b52b635106432da3ae6d06196
Vikram Mahadev B <vikram@collab.net>
2020-01-06 14:40:42 +0530

    S-67873 - Minor change missed earlier.

--- ctmcommands/admin/testmsghub.py
+++ ctmcommands/admin/testmsghub.py
@@ -26,7 +26,7 @@ _To send an email to an email address_
 
     def main(self):
         _url = "%s/pub/admin" % (self.server)
-        print(("Publishing a message to [%s]..." % (_url)))
+        print("Publishing a message to [%s]..." % (_url))
         ws = websocket.WebSocket(sslopt={"cert_reqs": 0})
         ws.connect(_url)
         msg = '''{
