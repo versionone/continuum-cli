cbaa0ade3633105a2947fca887a6893a9e4d0d96
Kevin Marsh <kmarsh@collab.net>
2020-02-13 17:46:28 -0500

    S-69949 plugin cli commands now accept a team
    -- configure plugin(s) does not need a team param so they are not edited here

--- ctmcommands/flow/testversionone.py
+++ ctmcommands/flow/testversionone.py
@@ -12,6 +12,9 @@ class TestVersionOne(ctmcommands.cmd.CSKCommand):
     Options = [Param(name='instance', short_name='i', long_name='instance',
                      optional=True, ptype='string',
                      doc='VersionOne instance name in the Continuum configuration. Optional, do not use if testing default VersionOne instance.'),
+               Param(name='team', short_name='t', long_name='team',
+                     optional=True, ptype='string',
+                     doc="The Team to search for the Plugin Instance to use. Defaults to plugins available to All Teams."),
                ]
 
     def main(self):
