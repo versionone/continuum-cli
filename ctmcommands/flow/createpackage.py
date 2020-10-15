f993d86651b6620adf3d40154753d80da45dff6b
C. Michael Pilato <cmpilato@collab.net>
2020-10-15 14:48:15 -0400

    [S-73554] Introduce 'ctm-create-package' CLI command

---
+++ ctmcommands/flow/createpackage.py
@@ -0,0 +1,35 @@
+#########################################################################
+#
+# Copyright 2019 VersionOne
+# All Rights Reserved.
+# http://www.versionone.com
+#
+#
+#########################################################################
+
+import ctmcommands.cmd
+from ctmcommands.param import Param
+
+
+class CreatePackage(ctmcommands.cmd.CSKCommand):
+
+    Description = """Creates a Package.
+
+Returns a Package Definition Object."""
+
+    API = 'create_package'
+    Examples = ''''''
+    Options = [Param(name='name', short_name='n', long_name='name',
+                     optional=False, ptype='string',
+                     doc='A name for the Package Definition.'),
+               Param(name='team', short_name='t', long_name='team',
+                     optional=False, ptype='string',
+                     doc='Team which the Package Definition should belong to'),
+               Param(name='description', short_name='d', long_name='description',
+                     optional=True, ptype='string',
+                     doc='A description for the Package Definition.')
+               ]
+
+    def main(self):
+        results = self.call_api(self.API, ['name', 'team', 'description'])
+        print(results)
